3c7f1cdb252e0e8536cc558e1e8ee3c0e86792b4
nowszhao <nowszhao@gmail.com>
2024-12-29 11:11:44 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 3
-        versionName = "1.0.3"
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
